da3897b34db6a9533569fc2e5eb56fba2f2ce0ac
yliu-fe <34157793+yliu-fe@users.noreply.github.com>
2024-07-26 17:14:47 +0800

    Update Collatz.java for Lab 1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,27 +1,29 @@
-/** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+/**
+ * Class that prints the Collatz sequence starting from a given number.
+ * 
+ * @author Yi Liu
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
-    public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
-        }
+  public static int nextNumber(int n) {
+    /**
+     * Returns the next number of Collatz sequence
+     * fixing the bug of the original skeleton code.
+     */
+    if (n % 2 == 0) {
+      return n / 2;
+    } else {
+      return 3 * n + 1;
     }
+  }
 
-    public static void main(String[] args) {
-        int n = 5;
-        System.out.print(n + " ");
-        while (n != 1) {
-            n = nextNumber(n);
-            System.out.print(n + " ");
-        }
-        System.out.println();
+  public static void main(String[] args) {
+    int n = 5;
+    System.out.print(n + " ");
+    while (n != 1) {
+      n = nextNumber(n);
+      System.out.print(n + " ");
     }
+    System.out.println();
+  }
 }
-
